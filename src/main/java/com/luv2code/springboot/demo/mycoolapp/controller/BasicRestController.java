b2f810743a0ed6813524ccb64363c418e5657561
Matheus Lourenço <matheuslourenco111@gmail.com>
2024-07-23 23:16:39 -0300

    mostra como usar valores definidos no .properties no codigo

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/BasicRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/controller/BasicRestController.java
@@ -1,10 +1,17 @@
 package com.luv2code.springboot.demo.mycoolapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BasicRestController {
+
+    @Value("${coach.name}")
+    private String nomeTreinador;
+
+    @Value("${team.name}")
+    private String nomeTime;
 
     @GetMapping("/")
     public String helloWorld(){
@@ -14,6 +21,16 @@ public class BasicRestController {
     @GetMapping("/devtools")
     public String devtools(){
         return "devtools está atualizando automaticamente";
+    }
+
+    @GetMapping("/nomeTreinador")
+    public String nomeTreinador(){
+        return nomeTreinador;
+    }
+
+    @GetMapping("/nomeTime")
+    public String nomeTime(){
+        return nomeTime;
     }
 
 
